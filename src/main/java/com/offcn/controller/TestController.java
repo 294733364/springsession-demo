40d08eb2105c518958ff217c39c6ef318cc5a188
ligang <294733364@qq.com>
2020-12-31 17:15:43 +0800

    添加了问好功能

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.offcn.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
@@ -17,5 +18,9 @@ public class TestController {
     @GetMapping("/get")
     public String getSession(HttpSession session){
         return (String)session.getAttribute("msg");
+    }
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello everyone!";
     }
 }
